461678e76a8592b77320a75a2d3840c6c9d8d6de
Roooohhhaaaannnn <rohan.sharma2467@gmail.com>
2024-08-21 00:06:45 +0530

    model training

--- src/logger.py
+++ src/logger.py
@@ -1,23 +1,15 @@
-import logging 
-import os 
+import logging
+import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-log_path= os.path.join(os.getcwd(),"logs",LOG_FILE)
-
-os.makedirs(LOG_FILE,exist_ok=True)
-
-log_file_path=os.path.join(log_path,LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-
-    filename=log_file_path,
-    format="[%(asctime)s] %(lineno)d %(name)s %(levelname)s %(message)s",
-
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-if __name__=="__main__":
-    logging.info("log started")
-
